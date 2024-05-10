62a7bd0788bfdfb85f9ec8699f0f51ea98a3ca42
Testerhubplayer <157597648+Testerhubplayer@users.noreply.github.com>
2024-05-10 15:44:41 +0800

    Delete Ability_Wars_loaders.lua

--- Ability_Wars_loaders.lua
+++
@@ -1,6 +0,0 @@
-local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/Testerhubplayer/Ability-wars/main/Ability_Wars_Games.lua'))()
-if games[game.PlaceId] then
-  loadstring(game:HttpGetAsync(games[game.PlaceId]))() 
-else 
-  warn("Tester hub currently does not support this experience!") 
-end
