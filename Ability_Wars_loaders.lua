d3898f1665ef7795b37363c7bf436e1e22bf616f
Testerhubplayer <157597648+Testerhubplayer@users.noreply.github.com>
2024-05-10 15:34:25 +0800

    Update Ability_Wars_loaders.lua

--- Ability_Wars_loaders.lua
+++ Ability_Wars_loaders.lua
@@ -1,1 +1,8 @@
-
+local games = loadstring(game:HttpGet('https://github.com/Testerhubplayer/Ability-wars/blob/main/Ability_Wars_loaders.lua'))()
+if games[game.PlaceId] then
+  local getgenv = getgenv or getfenv
+  getgenv().TesterLib = 'https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua'
+  loadstring(game:HttpGetAsync(games[game.PlaceId]))() 
+else 
+  warn("Voxul currently does not support this experience!") 
+end
