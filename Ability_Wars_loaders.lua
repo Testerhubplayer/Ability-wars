33021c751e45e3c28ef790446ee1d61c2fbca5b0
Testerhubplayer <157597648+Testerhubplayer@users.noreply.github.com>
2024-05-10 15:41:48 +0800

    Update Ability_Wars_loaders.lua

--- Ability_Wars_loaders.lua
+++ Ability_Wars_loaders.lua
@@ -1,7 +1,5 @@
 local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/Testerhubplayer/Ability-wars/main/Ability_Wars_Games.lua'))()
 if games[game.PlaceId] then
-  local getgenv = getgenv or getfenv
-  getgenv().TesterLib = 'https://raw.githubusercontent.com/Giangplay/Script/main/Orion_Library_PE_V2.lua'
   loadstring(game:HttpGetAsync(games[game.PlaceId]))() 
 else 
   warn("Tester hub currently does not support this experience!") 
